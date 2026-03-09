be6e2276c172566eb88d09fea84251ac90245435
Saravana Kumaar <saravanakumaar023@gmail.com>
2026-03-09 20:15:10 +0530

    Add script to reorganize files and standardize naming conventions

---
+++ reorganize_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Renaming files to standardize naming convention
+
+# Remove duplicate README file
+rm -f 1_README.md
+
+# Rename files
+mv 2_problem_statement.md 02_problem_statement.md
+mv 3_schema_design.sql 03_schema_design.sql
+mv 🔍_insights.md 06_insights.txt
